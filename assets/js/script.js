dee78482d295f9c43ed52a44f3650e45474cd866
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2023-11-07 12:43:11 +0000

    feat: add favicon and tidy ups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   }
 
+  document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  })
+
   runGame("addition");
 
 });
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
 * and after the user's answer has been processed
 */
 function runGame(gameType) {
+
+  document.getElementById("answer-box").value = "";
+  document.getElementById("answer-box").focus();
 
   // Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
@@ -43,7 +52,7 @@ function runGame(gameType) {
 }
 
 /**
-* Checks the answer agaist the first element in
+* Checks the answer against the first element in
 * the returned calculateCorrectAnswer array
 */
 function checkAnswer() {
